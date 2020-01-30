ac2d2b9ac2fe92d9a84abe7fad129e8f91826801
SNN <snndmr@users.noreply.github.com>
2020-01-30 14:37:50 +0300

    Update for BGR to RGB conversion

--- 1 - Images/episode 2.py
+++ 1 - Images/episode 2.py
@@ -2,6 +2,11 @@ import cv2
 from matplotlib import pyplot as plt
 
 img = cv2.imread('img.jpg', 1)
+
+# OpenCV follows BGR order, while matplotlib likely follows RGB order.
+# So, we need conversion between BGR and RGB.
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # img = img[:, :, ::-1]
+
 plt.imshow(img, cmap='bone', interpolation='hermite')
 
 # To hide tick values on X and Y axis
