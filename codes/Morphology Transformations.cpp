449dbdd20b85d631686f97bfecca33db3ee63f29
SNN <snndmr@users.noreply.github.com>
2020-05-20 21:49:19 +0300

    Update Morphology Transformations.cpp

--- codes/Morphology Transformations.cpp
+++ codes/Morphology Transformations.cpp
@@ -16,41 +16,24 @@ int posOfType = 0;
 int posOfShape = 0;
 int posOfKernel = 0;
 
-void update(Mat image) {
+void onChange(int, void* userdata) {
 	Mat instant;
 	Mat element = getStructuringElement(MorphShapes(posOfShape), Size(2 * posOfKernel + 1, 2 * posOfKernel + 1), Point(posOfKernel, posOfKernel));
-	morphologyEx(image, instant, MorphTypes(posOfType), element);
+	morphologyEx(*((Mat*) userdata), instant, MorphTypes(posOfType), element);
 	imshow(WINDOW_MAIN, instant);
-}
-
-void onChangeType(int pos, void* userdata) {
-	posOfType = pos;
-	update(*((Mat*) userdata));
-}
-
-void onChangeShape(int pos, void* userdata) {
-	posOfShape = pos;
-	update(*((Mat*) userdata));
-}
-
-void onChangeKernel(int pos, void* userdata) {
-	posOfKernel = pos;
-	update(*((Mat*) userdata));
 }
 
 int main(int argc, char** argv) {
 	Mat image = imread(argv[1]);
 
-	if(!image.data) {
+	if(image.empty()) {
 		return EXIT_FAILURE;
 	}
 
 	namedWindow(WINDOW_MAIN, WINDOW_AUTOSIZE);
-
-	createTrackbar(TRACKBAR_TYPE, WINDOW_MAIN, &posOfType, MAX_POS_TYPE, onChangeType, &image);
-	createTrackbar(TRACKBAR_SHAPE, WINDOW_MAIN, &posOfShape, MAX_POS_SHAPE, onChangeShape, &image);
-	createTrackbar(TRACKBAR_KERNEL, WINDOW_MAIN, &posOfKernel, MAX_POS_KERNEL, onChangeKernel, &image);
-
+	createTrackbar(TRACKBAR_TYPE, WINDOW_MAIN, &posOfType, MAX_POS_TYPE, onChange, &image);
+	createTrackbar(TRACKBAR_SHAPE, WINDOW_MAIN, &posOfShape, MAX_POS_SHAPE, onChange, &image);
+	createTrackbar(TRACKBAR_KERNEL, WINDOW_MAIN, &posOfKernel, MAX_POS_KERNEL, onChange, &image);
 	imshow(WINDOW_MAIN, image);
 	waitKey(0);
 	return EXIT_SUCCESS;
