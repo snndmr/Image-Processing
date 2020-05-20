f838f474c734a81d7212f8774cd2cfa46f75431a
SNN <snndmr@users.noreply.github.com>
2020-05-20 21:58:49 +0300

    Update Erosion & Dilaliton.cpp

--- codes/Erosion & Dilaliton.cpp
+++ codes/Erosion & Dilaliton.cpp
@@ -16,51 +16,33 @@ int posOfType = 0;
 int posOfKernel = 0;
 int posOfElement = 0;
 
-void update(Mat image) {
+void onChange(int, void* userdata) {
 	Mat instant;
 	Mat element = getStructuringElement(MorphShapes(posOfElement), Size(2 * posOfKernel + 1, 2 * posOfKernel + 1), Point(posOfKernel, posOfKernel));
 
 	switch(MorphTypes(posOfType)) {
 		case MORPH_ERODE:
-			erode(image, instant, element);
+			erode(*((Mat*) userdata), instant, element);
 			break;
 		case MORPH_DILATE:
-			dilate(image, instant, element);
+			dilate(*((Mat*) userdata), instant, element);
 			break;
 	}
 	imshow(WINDOW_MAIN, instant);
 }
 
-void onChangeType(int pos, void* userdata) {
-	posOfType = pos;
-	update(*((Mat*) userdata));
-}
-
-void onChangeKernel(int pos, void* userdata) {
-	posOfKernel = pos;
-	update(*((Mat*) userdata));
-}
-
-void onChangeElement(int pos, void* userdata) {
-	posOfElement = pos;
-	update(*((Mat*) userdata));
-}
-
 int main(int argc, char** argv) {
-	Mat image = imread(argv[1]);
+	Mat source = imread(argv[1]);
 
-	if(!image.data) {
+	if(source.empty()) {
 		return EXIT_FAILURE;
 	}
 
 	namedWindow(WINDOW_MAIN, WINDOW_AUTOSIZE);
-
-	createTrackbar(TRACKBAR_TYPE, WINDOW_MAIN, &posOfType, MAX_POS_TYPE, onChangeType, &image);
-	createTrackbar(TRACKBAR_KERNEL, WINDOW_MAIN, &posOfKernel, MAX_POS_KERNEL, onChangeKernel, &image);
-	createTrackbar(TRACKBAR_ELEMENT, WINDOW_MAIN, &posOfElement, MAX_POS_ELEMENT, onChangeElement, &image);
-
-	imshow(WINDOW_MAIN, image);
+	createTrackbar(TRACKBAR_TYPE, WINDOW_MAIN, &posOfType, MAX_POS_TYPE, onChange, &source);
+	createTrackbar(TRACKBAR_KERNEL, WINDOW_MAIN, &posOfKernel, MAX_POS_KERNEL, onChange, &source);
+	createTrackbar(TRACKBAR_ELEMENT, WINDOW_MAIN, &posOfElement, MAX_POS_ELEMENT, onChange, &source);
+	imshow(WINDOW_MAIN, source);
 	waitKey(0);
-
 	return EXIT_SUCCESS;
 }
