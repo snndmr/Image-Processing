3ec9c477953650b38c4c94efd2725d3c4f75b40b
snndmr <snndmrprx@gmail.com>
2020-05-28 13:21:26 +0300

    Last Update

--- codes/Object Classification.cpp
+++ codes/Object Classification.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 #include <opencv2/ml.hpp>
 #include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
 using namespace cv::ml;
 
+const int MIN_AREA = 500;
+
+const char *WINDOW_NAME_ORIGINAL = "Original";
+const char *WINDOW_NAME_AFTER = "After SVM and AOI";
 const char *pathOfTuerca = "data/nut/tuerca_%04d.pgm";
 const char *pathOfArandela = "data/ring/arandela_%04d.pgm";
 const char *pathOfTornillo = "data/screw/tornillo_%04d.pgm";
 
+Scalar randomColor() {
+	return Scalar(rand() % 255, rand() % 255, rand() % 255);
+}
+
 Mat removeLight(Mat image, Mat pattern) {
 	Mat output, frame32, pattern32;
-
 	// The division method requires a 32-bit float of images.
 	image.convertTo(frame32, CV_32F);
 	pattern.convertTo(pattern32, CV_32F);
-
 	// Formula: output = 255 * 1 - (frame/pattern)
 	output = 1 - (frame32 / pattern32);
 	output.convertTo(output, CV_8U, 255);
@@ -33,25 +39,21 @@ Mat preprocess(Mat image, Mat pattern) {
 	return image;
 }
 
-vector<vector<float>> ExtractFeatures(Mat pre, vector<int> *left = NULL, vector<int> *top = NULL) {
+vector<vector<float>> ExtractFeatures(Mat pre, vector<int> *xOfCenter = NULL, vector<int> *yOfCenter = NULL) {
+	vector<Vec4i> hierarchy;
 	vector<vector<float>> output;
 	vector<vector<Point>> contours;
-	Mat input = pre.clone();
-
-	vector<Vec4i> hierarchy;
 
 	findContours(pre, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
 
-	if(contours.empty()) {
-		return output;
-	}
+	if(contours.empty()) return output;
 
 	for(int i = 0; i < contours.size(); i++) {
 		Mat mask = Mat::zeros(pre.rows, pre.cols, CV_8UC1);
 		drawContours(mask, contours, i, Scalar(1), FILLED, LINE_8, hierarchy, 1);
 
 		float area = sum(mask)[0];
-		if(area > 500) {
+		if(area > MIN_AREA) {
 			RotatedRect rect = minAreaRect(contours[i]);
 			float width = rect.size.width;
 			float height = rect.size.height;
@@ -60,14 +62,13 @@ vector<vector<float>> ExtractFeatures(Mat pre, vector<int> *left = NULL, vector<
 			vector<float> row;
 			row.push_back(area);
 			row.push_back(aspectRatio);
-
 			output.push_back(row);
 
-			if(left != NULL) {
-				left->push_back((int) rect.center.x);
+			if(xOfCenter != NULL) {
+				xOfCenter->push_back((int) rect.center.x);
 			}
-			if(top != NULL) {
-				top->push_back((int) rect.center.y);
+			if(yOfCenter != NULL) {
+				yOfCenter->push_back((int) rect.center.y);
 			}
 		}
 	}
@@ -78,8 +79,7 @@ bool readFolderAndExtractFeatures(const char *folder, Mat pattern, int label, in
 								  vector<float> &trainingData, vector<int> &responsesData,
 								  vector<float> &testData, vector<float> &testResponsesData) {
 	VideoCapture images;
-
-	if(images.open(folder) == false) {
+	if(!images.open(folder)) {
 		cout << "Can not open the folder images" << endl;
 		return false;
 	}
@@ -106,28 +106,29 @@ bool readFolderAndExtractFeatures(const char *folder, Mat pattern, int label, in
 }
 
 Ptr<SVM> trainAndTest(Mat pattern) {
-	vector< float > trainingData;
-	vector< int > responsesData;
-	vector< float > testData;
-	vector< float > testResponsesData;
+	vector<float> trainingData;
+	vector<int> responsesData;
+	vector<float> testData;
+	vector<float> testResponsesData;
 
 	int numberOfTest = 20;
 
-	readFolderAndExtractFeatures(pathOfTuerca, pattern, 0, numberOfTest, trainingData, responsesData, testData, testResponsesData);
-	readFolderAndExtractFeatures(pathOfArandela, pattern, 1, numberOfTest, trainingData, responsesData, testData, testResponsesData);
-	readFolderAndExtractFeatures(pathOfTornillo, pattern, 2, numberOfTest, trainingData, responsesData, testData, testResponsesData);
+	readFolderAndExtractFeatures(pathOfTuerca, pattern, 0, numberOfTest,
+								 trainingData, responsesData, testData, testResponsesData);
+	readFolderAndExtractFeatures(pathOfArandela, pattern, 1, numberOfTest,
+								 trainingData, responsesData, testData, testResponsesData);
+	readFolderAndExtractFeatures(pathOfTornillo, pattern, 2, numberOfTest,
+								 trainingData, responsesData, testData, testResponsesData);
 
-	cout << "\n Num of train samples: " << responsesData.size() << endl;
-	cout << " Num of test samples: " << testResponsesData.size() << endl;
+	cout << endl << format(" Number of train samples: %4zd", responsesData.size());
+	cout << endl << format(" Number of test samples : %4zd", testResponsesData.size());
 
-	Mat trainingDataMat(trainingData.size() / 2, 2, CV_32FC1, &trainingData[0]);
-	Mat responses(responsesData.size(), 1, CV_32SC1, &responsesData[0]);
-
-	Mat testDataMat(testData.size() / 2, 2, CV_32FC1, &testData[0]);
-	Mat testResponses(testResponsesData.size(), 1, CV_32FC1, &testResponsesData[0]);
+	Mat trainingDataMat((int) trainingData.size() / 2, 2, CV_32FC1, &trainingData[0]);
+	Mat responses((int) responsesData.size(), 1, CV_32SC1, &responsesData[0]);
+	Mat testDataMat((int) testData.size() / 2, 2, CV_32FC1, &testData[0]);
+	Mat testResponses((int) testResponsesData.size(), 1, CV_32FC1, &testResponsesData[0]);
 
 	Ptr<TrainData> tdata = TrainData::create(trainingDataMat, ROW_SAMPLE, responses);
-
 	Ptr<SVM> svm = SVM::create();
 	svm->setType(SVM::C_SVC);
 	svm->setNu(0.05);
@@ -141,18 +142,16 @@ Ptr<SVM> trainAndTest(Mat pattern) {
 		// Test the ML Model
 		Mat testPredict;
 		svm->predict(testDataMat, testPredict);
-
 		// Error calculation
 		Mat errorMat = testPredict != testResponses;
-		float error = 100.0f * countNonZero(errorMat) / testResponsesData.size();
-		cout << " Error: " << error << "%" << endl;
+		cout << endl << format(" Error: %.5lf%%", 100.0f * countNonZero(errorMat) / testResponsesData.size());
 	}
 	return svm;
 }
 
 int main(int argc, const char **argv) {
 	if(argc != 3) {
-		cout << "You must enter image and pattern as arguments" << endl;
+		cout << " You must enter image and pattern as arguments" << endl;
 		return EXIT_FAILURE;
 	}
 
@@ -173,26 +172,25 @@ int main(int argc, const char **argv) {
 	Ptr<SVM> svm = trainAndTest(pattern);
 
 	Mat pre = preprocess(image, pattern);
+	vector<int> xOfCenter, yOfCenter;
+	vector<vector<float>> features = ExtractFeatures(pre, &xOfCenter, &yOfCenter);
 
-	vector<int> pos_left, pos_top;
-	vector<vector<float>> features = ExtractFeatures(pre, &pos_left, &pos_top);
-	cout << " Number of objects detected: " << features.size() << endl;
-	imshow("asd", image);
+	cout << endl << format(" Number of objects detected: %zd", features.size());
+	imshow(WINDOW_NAME_ORIGINAL, image);
 	for(int i = 0; i < features.size(); i++) {
 		Mat trainingDataMat(1, 2, CV_32FC1, &features[i][0]);
 		float result = svm->predict(trainingDataMat);
 
-		stringstream ss;
 		if(result == 0) {
-			putText(image, "Nut", Point2d(pos_left[i], pos_top[i]), FONT_HERSHEY_DUPLEX, .5, Scalar(255, 255, 0));
+			putText(image, "Nut", Point2d(xOfCenter[i], yOfCenter[i]), FONT_HERSHEY_TRIPLEX, .5, randomColor());
 		} else if(result == 1) {
-			putText(image, "Ring", Point2d(pos_left[i], pos_top[i]), FONT_HERSHEY_DUPLEX, .5, Scalar(0, 255, 255));
+			putText(image, "Ring", Point2d(xOfCenter[i], yOfCenter[i]), FONT_HERSHEY_TRIPLEX, .5, randomColor());
 		} else if(result == 2) {
-			putText(image, "Screw", Point2d(pos_left[i], pos_top[i]), FONT_HERSHEY_DUPLEX, .5, Scalar(255, 0, 255));
+			putText(image, "Screw", Point2d(xOfCenter[i], yOfCenter[i]), FONT_HERSHEY_TRIPLEX, .5, randomColor());
 		}
 	}
-	namedWindow("Image", WINDOW_FREERATIO);
-	imshow("Image", image);
+	namedWindow(WINDOW_NAME_AFTER, WINDOW_AUTOSIZE);
+	imshow(WINDOW_NAME_AFTER, image);
 	waitKey(0);
 	return EXIT_SUCCESS;
 }
