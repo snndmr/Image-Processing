069ec508a14e6ce50660df9ccb252b87501d7e51
SNN <53225314+snndmr@users.noreply.github.com>
2020-06-03 16:11:15 +0300

    ml logic added

--- codes/Object Classification.cpp
+++ codes/Object Classification.cpp
@@ -1,59 +1,31 @@
 #include <iostream>
 #include <opencv2/ml.hpp>
-#include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-using namespace std;
 using namespace cv;
 using namespace cv::ml;
+using namespace std;
 
-const int MIN_AREA = 500;
+const char *pathOfDataFile = "data.xml";
+const char *pathOfTest = "data/test/*.png";
+const char *pathOfTrain = "data/train/*.png";
 
-const char *WINDOW_NAME_ORIGINAL = "Original";
-const char *WINDOW_NAME_AFTER = "After SVM and AOI";
-const char *pathOfTuerca = "data/nut/tuerca_%04d.pgm";
-const char *pathOfArandela = "data/ring/arandela_%04d.pgm";
-const char *pathOfTornillo = "data/screw/tornillo_%04d.pgm";
-
-Scalar randomColor() {
-	return Scalar(rand() % 255, rand() % 255, rand() % 255);
-}
-
-Mat removeLight(Mat image, Mat pattern) {
-	Mat output, frame32, pattern32;
-	// The division method requires a 32-bit float of images.
-	image.convertTo(frame32, CV_32F);
-	pattern.convertTo(pattern32, CV_32F);
-	// Formula: output = 255 * 1 - (frame/pattern)
-	output = 1 - (frame32 / pattern32);
-	output.convertTo(output, CV_8U, 255);
-	return output;
-}
-
-Mat preprocess(Mat image, Mat pattern) {
-	if(image.channels() == 3) cvtColor(image, image, COLOR_RGB2GRAY);
-	medianBlur(image, image, 3);
-	image = removeLight(image, pattern);
-	threshold(image, image, 30, 255, THRESH_BINARY);
-	return image;
-}
-
-vector<vector<float>> ExtractFeatures(Mat pre, vector<int> *xOfCenter = NULL, vector<int> *yOfCenter = NULL) {
+vector<vector<float>> extractFeatures(Mat image) {
 	vector<Vec4i> hierarchy;
 	vector<vector<float>> output;
 	vector<vector<Point>> contours;
 
-	findContours(pre, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
+	findContours(image, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_SIMPLE);
 
 	if(contours.empty()) return output;
 
 	for(int i = 0; i < contours.size(); i++) {
-		Mat mask = Mat::zeros(pre.rows, pre.cols, CV_8UC1);
+		Mat mask = Mat::zeros(image.rows, image.cols, CV_8UC1);
 		drawContours(mask, contours, i, Scalar(1), FILLED, LINE_8, hierarchy, 1);
 
-		float area = sum(mask)[0];
-		if(area > MIN_AREA) {
+		float area = (float) sum(mask)[0];
+		if(area > 500) {
 			RotatedRect rect = minAreaRect(contours[i]);
 			float width = rect.size.width;
 			float height = rect.size.height;
@@ -63,72 +35,94 @@ vector<vector<float>> ExtractFeatures(Mat pre, vector<int> *xOfCenter = NULL, ve
 			row.push_back(area);
 			row.push_back(aspectRatio);
 			output.push_back(row);
-
-			if(xOfCenter != NULL) {
-				xOfCenter->push_back((int) rect.center.x);
-			}
-			if(yOfCenter != NULL) {
-				yOfCenter->push_back((int) rect.center.y);
-			}
 		}
 	}
 	return output;
 }
 
-bool readFolderAndExtractFeatures(const char *folder, Mat pattern, int label, int numberOfTest,
-								  vector<float> &trainingData, vector<int> &responsesData,
-								  vector<float> &testData, vector<float> &testResponsesData) {
-	VideoCapture images;
-	if(!images.open(folder)) {
-		cout << "Can not open the folder images" << endl;
-		return false;
-	}
+template <class _Ty>
+void readAndExtractFeatures(const char *pathOfFolder, vector<float> &data, vector<_Ty> &dataResponse) {
+	vector<string> fileNames;
+	glob(pathOfFolder, fileNames);
 
-	cout << endl;
-	Mat frame;
-	for(int imageIndex = 0; images.read(frame); imageIndex += 1) {
-		cout << format("\r %03d captured from %s", imageIndex, folder);
-		Mat pre = preprocess(frame, pattern);
-		vector<vector<float>> features = ExtractFeatures(pre);
+	cout << format("\n Reading started in the %s folder\n", pathOfFolder);
+	for(int imageIndex = 0; imageIndex < fileNames.size(); imageIndex++) {
+		Mat image = imread(fileNames.at(imageIndex), IMREAD_GRAYSCALE);
+		threshold(image, image, 90, 255, THRESH_BINARY);
+
+		vector<vector<float>> features = extractFeatures(image);
 		for(int i = 0; i < features.size(); i++) {
-			if(imageIndex >= numberOfTest) {
-				trainingData.push_back(features[i][0]);
-				trainingData.push_back(features[i][1]);
-				responsesData.push_back(label);
-			} else {
-				testData.push_back(features[i][0]);
-				testData.push_back(features[i][1]);
-				testResponsesData.push_back(label);
-			}
+			data.push_back(features[i][0]);
+			data.push_back(features[i][1]);
+			dataResponse.push_back((_Ty) fileNames[imageIndex][fileNames[imageIndex].size() - 6] - 48);
 		}
+		cout << format("\r %d out of %d were process. (%.2f%%) ",
+					   imageIndex + 1, fileNames.size(), 100 * ((float) imageIndex + 1) / fileNames.size());
 	}
-	return true;
+	cout << endl;
 }
 
-Ptr<SVM> trainAndTest(Mat pattern) {
-	vector<float> trainingData;
-	vector<int> responsesData;
-	vector<float> testData;
-	vector<float> testResponsesData;
+bool readFromFile(Mat &testMat, Mat &trainMat, Mat &testResponses, Mat &trainResponses) {
+	FileStorage fileRead(pathOfDataFile, cv::FileStorage::READ);
 
-	int numberOfTest = 20;
+	if(fileRead.isOpened()) {
+		cout << format("\n Reading started in the %s data file\n", pathOfDataFile);
 
-	readFolderAndExtractFeatures(pathOfTuerca, pattern, 0, numberOfTest,
-								 trainingData, responsesData, testData, testResponsesData);
-	readFolderAndExtractFeatures(pathOfArandela, pattern, 1, numberOfTest,
-								 trainingData, responsesData, testData, testResponsesData);
-	readFolderAndExtractFeatures(pathOfTornillo, pattern, 2, numberOfTest,
-								 trainingData, responsesData, testData, testResponsesData);
+		fileRead["testMat"] >> testMat;
+		fileRead["trainMat"] >> trainMat;
+		fileRead["testResponses"] >> testResponses;
+		fileRead["trainResponses"] >> trainResponses;
 
-	cout << endl << format(" Number of train samples: %4zd", responsesData.size());
-	cout << endl << format(" Number of test samples : %4zd", testResponsesData.size());
+		return true;
+	}
+	return false;
+}
 
-	Mat trainingDataMat((int) trainingData.size() / 2, 2, CV_32FC1, &trainingData[0]);
-	Mat responses((int) responsesData.size(), 1, CV_32SC1, &responsesData[0]);
-	Mat testDataMat((int) testData.size() / 2, 2, CV_32FC1, &testData[0]);
-	Mat testResponses((int) testResponsesData.size(), 1, CV_32FC1, &testResponsesData[0]);
+void writeToFile(Mat &testMat, Mat &trainMat, Mat &testResponses, Mat &trainResponses) {
+	cout << format("\n Writing started to %s data file\n", pathOfDataFile);
 
-	Ptr<TrainData> tdata = TrainData::create(trainingDataMat, ROW_SAMPLE, responses);
+	FileStorage fileWrite("data.xml", cv::FileStorage::WRITE);
+	fileWrite << "testMat" << testMat;
+	fileWrite << "trainMat" << trainMat;
+	fileWrite << "testResponses" << testResponses;
+	fileWrite << "trainResponses" << trainResponses;
+
+	cout << format("\n %s data file created\n", pathOfDataFile);
+}
+
+void trainAndTest() {
+	Mat testMat;
+	Mat trainMat;
+	Mat testResponses;
+	Mat trainResponses;
+
+	if(!readFromFile(testMat, trainMat, testResponses, trainResponses)) {
+		cout << "\n Failed to open data.xml data file\n";
+
+		vector<float> test;
+		vector<float> train;
+		vector<float> testResponse;
+		vector<int> trainResponse;
+
+		readAndExtractFeatures(pathOfTest, test, testResponse);
+		readAndExtractFeatures(pathOfTrain, train, trainResponse);
+
+		testMat = Mat((int) test.size() / 2, 2, CV_32FC1, &test[0]);
+		trainMat = Mat((int) train.size() / 2, 2, CV_32FC1, &train[0]);
+		testResponses = Mat((int) testResponse.size(), 1, CV_32FC1, &testResponse[0]);
+		trainResponses = Mat((int) trainResponse.size(), 1, CV_32SC1, &trainResponse[0]);
+
+		writeToFile(testMat, trainMat, testResponses, trainResponses);
+	}
+
+	cout << format("\n Size of Test Mat: %d"
+				   "\n Size of Train Mat: %d"
+				   "\n Size of Test Responses: %d"
+				   "\n Size of Train Responses: %d",
+				   testMat.size().height, trainMat.size().height,
+				   testResponses.size().height, trainResponses.size().height);
+
+	Ptr<TrainData> trainData = TrainData::create(trainMat, ROW_SAMPLE, trainResponses);
 	Ptr<SVM> svm = SVM::create();
 	svm->setType(SVM::C_SVC);
 	svm->setNu(0.05);
@@ -136,61 +130,18 @@ Ptr<SVM> trainAndTest(Mat pattern) {
 	svm->setDegree(1.0);
 	svm->setGamma(2.0);
 	svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
-	svm->train(tdata);
+	svm->train(trainData);
 
-	if(testResponsesData.size() > 0) {
-		// Test the ML Model
+	if(testResponses.size().height > 0) {
 		Mat testPredict;
-		svm->predict(testDataMat, testPredict);
-		// Error calculation
+		svm->predict(testMat, testPredict);
 		Mat errorMat = testPredict != testResponses;
-		cout << endl << format(" Error: %.5lf%%", 100.0f * countNonZero(errorMat) / testResponsesData.size());
+
+		cout << format("\n\n Error: %.5lf%%\n", 100.0f * countNonZero(errorMat) / testResponses.size().height);
 	}
-	return svm;
 }
 
-int main(int argc, const char **argv) {
-	if(argc != 3) {
-		cout << " You must enter image and pattern as arguments" << endl;
-		return EXIT_FAILURE;
-	}
-
-	Mat image = imread(argv[1]);
-	Mat pattern = imread(argv[2], IMREAD_GRAYSCALE);
-
-	if(image.empty()) {
-		cout << "Error loading image " << argv[1] << endl;
-		return EXIT_FAILURE;
-	}
-
-	if(pattern.empty()) {
-		cout << "Error loading image " << argv[2] << endl;
-		return EXIT_FAILURE;
-	}
-	medianBlur(pattern, pattern, 3);
-
-	Ptr<SVM> svm = trainAndTest(pattern);
-
-	Mat pre = preprocess(image, pattern);
-	vector<int> xOfCenter, yOfCenter;
-	vector<vector<float>> features = ExtractFeatures(pre, &xOfCenter, &yOfCenter);
-
-	cout << endl << format(" Number of objects detected: %zd", features.size());
-	imshow(WINDOW_NAME_ORIGINAL, image);
-	for(int i = 0; i < features.size(); i++) {
-		Mat trainingDataMat(1, 2, CV_32FC1, &features[i][0]);
-		float result = svm->predict(trainingDataMat);
-
-		if(result == 0) {
-			putText(image, "Nut", Point2d(xOfCenter[i], yOfCenter[i]), FONT_HERSHEY_TRIPLEX, .5, randomColor());
-		} else if(result == 1) {
-			putText(image, "Ring", Point2d(xOfCenter[i], yOfCenter[i]), FONT_HERSHEY_TRIPLEX, .5, randomColor());
-		} else if(result == 2) {
-			putText(image, "Screw", Point2d(xOfCenter[i], yOfCenter[i]), FONT_HERSHEY_TRIPLEX, .5, randomColor());
-		}
-	}
-	namedWindow(WINDOW_NAME_AFTER, WINDOW_AUTOSIZE);
-	imshow(WINDOW_NAME_AFTER, image);
-	waitKey(0);
+int main(int argc, char **argv) {
+	trainAndTest();
 	return EXIT_SUCCESS;
 }
