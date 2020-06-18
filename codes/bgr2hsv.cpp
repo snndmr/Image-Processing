74cac51ebdd9854829dbd37ecc1835e888ea0faa
SNN <53225314+snndmr@users.noreply.github.com>
2020-06-18 17:36:50 +0300

    Update bgr2hsv.cpp

--- codes/bgr2hsv.cpp
+++ codes/bgr2hsv.cpp
@@ -5,14 +5,14 @@
 using namespace cv;
 using namespace std;
 
-const double HUE_TOLARENCE = 10.0;
-const double SAT_TOLARENCE = 30.0;
-const double VAL_TOLARENCE = 30.0;
+const Scalar TOLERANCE = Scalar(10, 36, 36);
+const String WINDOW_NAME_ORIGINAL = "Origininal";
+const String WINDOW_NAME_FIND_COLOR = "Find Color";
 
 struct Bound {
 	Scalar lower;
-	Scalar middle;
 	Scalar upper;
+	Scalar middle;
 };
 
 Mat findColor(Mat image, const Scalar &lowerBound, const Scalar &upperBound) {
@@ -30,13 +30,12 @@ Bound findBounds(Mat image) {
 
 	Bound boundary;
 	boundary.middle = mean(hsv);
-	boundary.lower = Scalar(boundary.middle[0] - HUE_TOLARENCE, boundary.middle[1] - SAT_TOLARENCE, boundary.middle[2] - VAL_TOLARENCE);
-	boundary.upper = Scalar(boundary.middle[0] + HUE_TOLARENCE, boundary.middle[1] + SAT_TOLARENCE, boundary.middle[2] + VAL_TOLARENCE);
+	boundary.lower = Scalar(boundary.middle) - TOLERANCE;
+	boundary.upper = Scalar(boundary.middle) + TOLERANCE;
 	return boundary;
 }
 
 Point init;
-
 void onMouse(int event, int x, int y, int flag, void *userdata) {
 	if(event == EVENT_LBUTTONDOWN) {
 		init = Point(x, y);
@@ -45,21 +44,25 @@ void onMouse(int event, int x, int y, int flag, void *userdata) {
 	if(flag == EVENT_LBUTTONDOWN) {
 		Mat instant = (*(Mat *) userdata).clone();
 		rectangle(instant, init, Point(x, y), Scalar(255, 255, 255), 2);
-		imshow("Original", instant);
+		imshow(WINDOW_NAME_ORIGINAL, instant);
 	}
 
 	if(event == EVENT_LBUTTONUP) {
 		Bound bound = findBounds((*(Mat *) userdata)(Rect(init, Point(x, y))));
-		imshow("Find Color", findColor(*(Mat *) userdata, bound.lower, bound.upper));
+		imshow(WINDOW_NAME_FIND_COLOR, findColor(*(Mat *) userdata, bound.lower, bound.upper));
 	}
 }
+
 int main(int argc, const char *argv[]) {
 	Mat image = imread(argv[1]);
-	Bound bound = findBounds(image);
+	if(image.empty()) {
+		cout << format("%s could not be opened!", argv[1]);
+		return EXIT_FAILURE;
+	}
 
-	namedWindow("Original", WINDOW_AUTOSIZE);
-	setMouseCallback("Original", onMouse, &image);
-	imshow("Original", image);
+	namedWindow(WINDOW_NAME_ORIGINAL, WINDOW_AUTOSIZE);
+	setMouseCallback(WINDOW_NAME_ORIGINAL, onMouse, &image);
+	imshow(WINDOW_NAME_ORIGINAL, image);
 	waitKey(0);
 	return EXIT_SUCCESS;
 }
